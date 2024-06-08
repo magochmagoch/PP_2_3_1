924c7f6cff0d5124e6e1ad4679c4ad89b01b577a
Магомед Ахмедов <magomed.ahmedov1993@mail.ru>
2024-06-08 20:15:50 +0300

    Инициализация списка перенесена в сервис Использовано внедрение зависимостей

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -5,5 +5,5 @@ import web.model.Car;
 import java.util.List;
 
 public interface CarService {
-    List<Car> getListOfCarsByCount(List<Car> cars, int count);
+    List<Car> getListOfCarsByCount(int count);
 }
